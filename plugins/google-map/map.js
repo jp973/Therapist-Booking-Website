1fb36533f03b2f66d0d6a3b2fce0be764d8f92da
jayaprakash <jayaprakashshettigar686@gmail.com>
2025-05-07 16:11:57 +0530

    Add files via upload

---
+++ plugins/google-map/map.js
@@ -0,0 +1,59 @@
+function initMap() {
+  // Styles a map in night mode.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 12.998090518609171, lng: 74.80875884983799},  // Your location coordinates
+    zoom: 12,
+    styles: [
+      {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+      {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+      {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+      {
+        featureType: 'administrative.locality',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#d59563'}]
+      },
+      {
+        featureType: 'poi',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#d59563'}]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'geometry',
+        stylers: [{color: '#263c3f'}]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#6b9a76'}]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry',
+        stylers: [{color: '#38414e'}]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry.stroke',
+        stylers: [{color: '#212a37'}]
+      },
+      {
+        featureType: 'road',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#9ca5b3'}]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry',
+        stylers: [{color: '#746855'}]
+      }
+    ]
+  });
+
+  // Add a marker for your location
+  var marker = new google.maps.Marker({
+    position: {lat: 12.998090518609171, lng: 74.80875884983799},  // Your location coordinates
+    map: map,
+    title: 'Surathkal'
+  });
+}
